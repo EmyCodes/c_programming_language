62d833ade90d133a948954abe0d410117e1c681c
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2022-09-22 07:22:26 +0000

    Modification on Oddlitl.c - Calculating size of an Array

--- basic/examples/oddlitl.c
+++ basic/examples/oddlitl.c
@@ -7,12 +7,14 @@ int main(void)
 	long x;
 	float f;
 	double d;
-	
+	char temp[8];
+
 	puts("Variables sizes:");
 	printf("Size of char variable c = %d\n", sizeof(c));
 	printf("Size of int variable i = %d\n", sizeof(i));
 	printf("Size of long variable x = %d\n", sizeof(x));
 	printf("Size of float variable f = %d\n", sizeof(f));
 	printf("Size of double variable d = %d\n", sizeof(d));
+	printf("Size of the temp array = %d\n", sizeof(temp));
 	return (0);
 }
