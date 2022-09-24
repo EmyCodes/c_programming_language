a17d6c55dac8dfa1ed7ad9c47828d3cb77c21a17
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2022-09-24 12:55:45 +0000

    Modification to hello.c file

--- basic/examples/hello.c
+++ basic/examples/hello.c
@@ -3,12 +3,13 @@
 int main(void)
 {
 	char array[] = "Hello!\n";
-	char *a;
+	char *a, *e;
 
 	printf("%s", array);
-
 	a = array;
+	e = &array[1];
 
 	printf("Array, 'array' is %d bytes long and lives in %p address.\n", sizeof(array), a);
+	printf("The second element of 'array' is %d bytes long and lives at %p address", sizeof(array[1]), e);
 	return (0);
 }
